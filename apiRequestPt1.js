9cd671ff3ab244795582ef04f70460646f47816b
Lauren Flavell <Lauren.Flavell@softwire.com>
2021-08-19 21:33:40 +0100

    print next 5 buses using bus stop number

---
+++ apiRequestPt1.js
@@ -0,0 +1,29 @@
+const fetch = require('node-fetch');
+const stopNumber = '490008660N'
+
+function sortBusArray(busArr) {
+    return busArr.sort((a, b) => a.timeToStation - b.timeToStation)
+}
+
+function printInfoAboutBuses(busArr) {
+    const sortedbusArr = sortBusArray(busArr)
+
+    console.log('Buses Due:') 
+
+    for (const bus in sortedbusArr) {
+        
+        if ([bus] < 5)
+        console.log(
+            `Bus ${sortedbusArr[bus].lineId} is due in ${Math.ceil(sortedbusArr[bus].timeToStation / 60)} minutes`)
+    }
+}
+
+function extractJsonFromResponse(response) {
+    return response.json();
+}
+
+fetch(`https://api.tfl.gov.uk/StopPoint/${stopNumber}/Arrivals?app_key=697968f7487e4271b29e648c72016a69`)
+    .then(extractJsonFromResponse)
+    .then(printInfoAboutBuses);
+
+
